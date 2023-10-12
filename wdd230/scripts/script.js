28461b0bd42fc5749fe50e5513ebc934a69b01ab
gcelesty <celestegeorge17@gmail.com>
2023-10-12 17:26:54 -0500

    hw

--- wdd230/scripts/script.js
+++ wdd230/scripts/script.js
@@ -5,3 +5,27 @@ button.addEventListener("click", () => {
   nav.classList.toggle("open");
   button.classList.toggle("open");
 });
+
+
+/*Dark mode*/
+const darkMode = document.querySelector("#darkMode");
+const main = document.querySelector("main");
+const a = document.querySelectorAll("main .card1 ul li a");
+
+darkMode.addEventListener("click", () => {
+  if (darkMode.textContent.includes("🕶️")) {
+    main.style.background = "#000";
+    main.style.color = "#fff";
+    a.forEach((link) => {
+      link.style.color = "#fff";
+    });
+    darkMode.textContent = "🔆";
+  }else {
+    main.style.background = "#fff";
+    main.style.color = "#000";
+    a.forEach((link) => {
+      link.style.color = "#000";
+    });
+    darkMode.textContent = "🕶️";
+  }
+});
