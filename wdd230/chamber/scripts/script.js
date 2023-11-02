4202080ca342f2e9a801e2962d26ab2c7e34a848
gcelesty <celestegeorge17@gmail.com>
2023-11-02 16:16:23 -0500

    hw

--- wdd230/chamber/scripts/script.js
+++ wdd230/chamber/scripts/script.js
@@ -1,3 +1,19 @@
+/* last modification date*/
+
+//Display the year at the bottom of the page
+const todaysDate = new Date();
+document.getElementById("year").textContent = todaysDate.getFullYear();
+
+// Display the last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
+
+//DropDown Menu Configuration
+const button = document.querySelector("#menu");
+const navList = document.querySelector("nav");
+button.addEventListener("click", () => {
+  navList.classList.toggle("open");
+});
+
 /* button */
 const menuButton = document.getElementById('menu');
 const navMenu = document.querySelector('nav');
@@ -41,3 +57,74 @@ if (localStorage.getItem("lastVisitDate")) {
     document.getElementById("last-visit").textContent =
       "Welcome! Let us know if you have any questions.";
   }
+
+  /*Form*/
+/*Time Stamp */
+
+function setTimestamp() {
+    const timestampElement = document.getElementById("timestamp");
+    const currentDate = new Date();
+    const formattedTimestamp = currentDate.toLocaleString();
+  }
+  setTimestamp();
+  
+  // Members Dinamically
+  
+  const cardContainer = document.querySelector("#card-container");
+  const membersUrl = "https://ggrados.github.io/wdd230/chamber/data/members.json";
+  
+  async function getMembers() {
+    try {
+      const response = await fetch(membersUrl);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        displayMembers(data);
+      } else {
+        throw Error(await response.text());
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  function displayMembers(membersData) {
+    membersData.companies.forEach((company) => {
+      const article = document.createElement("article");
+      article.classList.add("container");
+  
+      const h2 = document.createElement("h2");
+      h2.textContent = company.name;
+  
+      const address = document.createElement("p");
+      address.textContent = company.address;
+  
+      const phoneNumber = document.createElement("p");
+      phoneNumber.textContent = company.phoneNumber;
+  
+      const website = document.createElement("a");
+      website.setAttribute("href", company.websiteURL);
+      website.textContent = company.websiteURL;
+      website.classList.add("memberLink");
+  
+      const memberShipLevel = document.createElement("p");
+      memberShipLevel.textContent = `MemberShip Level: ${company.membershipLevel}`;
+  
+      const img = document.createElement("img");
+      img.setAttribute("src", company.image);
+  
+      article.appendChild(img);
+      article.appendChild(h2);
+      article.appendChild(address);
+      article.appendChild(phoneNumber);
+      article.appendChild(website);
+      article.appendChild(memberShipLevel);
+  
+      cardContainer.appendChild(article);
+    });
+  }
+  
+  if (cardContainer) {
+    getMembers();
+  }
+  
