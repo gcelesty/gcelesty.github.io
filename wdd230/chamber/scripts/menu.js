a8569e4a7f293e5af00927c42b8afb2bd3047490
gcelesty <celestegeorge17@gmail.com>
2023-11-26 00:50:25 -0600

    hw

--- wdd230/chamber/scripts/menu.js
+++ wdd230/chamber/scripts/menu.js
@@ -1,6 +1,20 @@
-const menuButton = document.getElementById('menu');
-const navLinksContainer = document.getElementById('navLinksContainer');
+// menu.js
 
-menuButton.addEventListener('click', function() {
-    navLinksContainer.classList.toggle('open');
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.getElementById("menu");
+    const navLinksContainer = document.getElementById("navLinksContainer");
+
+    // Add click event listener to the menu button
+    menuButton.addEventListener("click", function () {
+        // Toggle the visibility of the navigation links container
+        navLinksContainer.style.display = (navLinksContainer.style.display === "block") ? "none" : "block";
+    });
+
+    // Close the navigation links when a link is clicked
+    const navLinks = document.querySelectorAll("#navLinksContainer ol li a");
+    navLinks.forEach(function (link) {
+        link.addEventListener("click", function () {
+            navLinksContainer.style.display = "none";
+        });
+    });
 });
