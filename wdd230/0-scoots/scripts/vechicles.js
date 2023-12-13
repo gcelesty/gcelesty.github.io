9905af61504311528ec36f4114ae7e69b23c8af8
gcelesty <celestegeorge17@gmail.com>
2023-12-13 15:20:44 -0600

    final

--- wdd230/0-scoots/scripts/vechicles.js
+++ wdd230/0-scoots/scripts/vechicles.js
@@ -3,38 +3,46 @@ const linksURL = "https://gcelesty.github.io/wdd230/0-scoots/data/vechicle.json"
 
 async function getVehicles() {
     try {
-      const response = await fetch(linksURL);
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-        displayVehicles(data.vechicles);
-      } else {
-        throw Error(await response.text());
-      }
+        const response = await fetch(linksURL);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayVehicles(data.vechicles);
+        } else {
+            throw Error(await response.text());
+        }
     } catch (error) {
-      console.log(error);
+        console.log(error);
     }
-  }
-  
+}
+
 getVehicles();
 
 const tableDiv = document.getElementById("table");
 
 const displayVehicles = function (vehicles) {
     const table = document.createElement("table");
-  
+
+    // Create header row
+    const headerRow = document.createElement("tr");
+    for (const key in vehicles[0]) {
+        const headerCell = document.createElement("th");
+        headerCell.textContent = key;
+        headerRow.appendChild(headerCell);
+    }
+    table.appendChild(headerRow);
+
     // Create table rows for each vehicle
     vehicles.forEach((vehicle) => {
-      const row = document.createElement("tr");
-      for (const key in vehicle) {
-        const cell = document.createElement("td");
-        cell.textContent = vehicle[key];
-        row.appendChild(cell);
-      }
-      table.appendChild(row);
+        const row = document.createElement("tr");
+        for (const key in vehicle) {
+            const cell = document.createElement("td");
+            cell.textContent = vehicle[key];
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
     });
-  
+
     // Append the table to the tableDiv
     tableDiv.appendChild(table);
-  };
-  
+};
