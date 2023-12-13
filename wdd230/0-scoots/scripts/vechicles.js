d6c328bd0cc991fced9e88cb575c3c40be531771
gcelesty <celestegeorge17@gmail.com>
2023-12-13 15:48:32 -0600

    final

--- wdd230/0-scoots/scripts/vechicles.js
+++ wdd230/0-scoots/scripts/vechicles.js
@@ -1,48 +1,44 @@
-const baseURL = "https://gcelesty.github.io/wdd230/0-scoots/";
-const linksURL = "https://gcelesty.github.io/wdd230/0-scoots/data/vechicle.json";
-
-async function getVehicles() {
-    try {
-        const response = await fetch(linksURL);
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-            displayVehicles(data.vechicles);
-        } else {
-            throw Error(await response.text());
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-getVehicles();
-
+const url = "https://gcelesty.github.io/wdd230/0-scoots/data/vechicle.json";
 const tableDiv = document.getElementById("table");
 
-const displayVehicles = function (vehicles) {
-    const table = document.createElement("table");
+async function getVehicleData() {
+  try {
+    const response = await fetch(url);
 
-    // Create header row
-    const headerRow = document.createElement("tr");
-    for (const key in vehicles[0]) {
-        const headerCell = document.createElement("th");
-        headerCell.textContent = key;
-        headerRow.appendChild(headerCell);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    table.appendChild(headerRow);
 
-    // Create table rows for each vehicle
-    vehicles.forEach((vehicle) => {
-        const row = document.createElement("tr");
-        for (const key in vehicle) {
-            const cell = document.createElement("td");
-            cell.textContent = vehicle[key];
-            row.appendChild(cell);
-        }
-        table.appendChild(row);
+    const data = await response.json();
+    console.table(data.vechicles);
+    displayVehicles(data.vechicles);
+  } catch (error) {
+    console.error("Error fetching vehicle data:", error);
+  }
+}
+
+const displayVehicles = (vehicles) => {
+  // Create the table
+  const table = document.createElement("table");
+
+  // Create data rows
+  for (let i = 0; i < vehicles.length; i++) {
+    const vehicle = vehicles[i];
+    const row = document.createElement("tr");
+
+    // Create table cells for each property
+    Object.keys(vehicle).forEach((key) => {
+      const cell = document.createElement("td");
+      cell.textContent = vehicle[key];
+      row.appendChild(cell);
     });
 
-    // Append the table to the tableDiv
-    tableDiv.appendChild(table);
+    // Append the row to the table
+    table.appendChild(row);
+  }
+
+  // Append the table to the tableDiv
+  tableDiv.appendChild(table);
 };
+
+getVehicleData();
